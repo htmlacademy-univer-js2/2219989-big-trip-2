f05dd8f73b471bd4020ff821de6ef613d499a06c
Софья Доровских <sofya_dorovskikh@MacBook-Air-Sofa.local>
2023-06-11 20:50:59 +0500

    Меняй-удаляй

--- src/const.js
+++ src/const.js
@@ -1,3 +1,8 @@
+const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PAST: 'past'
+};
 const SortType = {
   DAY: 'day',
   TIME: 'time',
@@ -6,4 +11,16 @@ const SortType = {
   OFFER: 'offer'
 };
 
-export { SortType };
+const UserAction = {
+  UPDATE_POINT: 'UPDATE_POINT',
+  ADD_POINT: 'ADD_POINT',
+  DELETE_POINT: 'DELETE_POINT',
+};
+
+const UpdateType = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+};
+
+export { FilterType, SortType, UserAction, UpdateType };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import FiltersView from './view/filters-view.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import TripEventsPresenter from './presenter/trip.js';
+import FilterModel from './model/filter-model.js';
+import NewPointButtonView from './view/new-point-button-view.js';
 import PointsModel from './model/trip-points-model.js';
+import SiteMenuView from './view/site-menu-view.js';
 import { getPoints, getDestinations, getOffers } from './fish-data/point.js';
 import { render } from './framework/render.js';
-import { generateFilter } from './fish-data/filter.js';
 
 const siteHeaderElement = document.querySelector('.trip-main');
 const siteMainElement = document.querySelector('.page-main');
@@ -13,9 +15,24 @@ const destinations = getDestinations();
 
 const pointsModel = new PointsModel();
 pointsModel.init(points, destinations, offersByType);
-const tripPresenter = new TripEventsPresenter(siteMainElement.querySelector('.trip-events'), pointsModel);
-tripPresenter.init();
+const filterModel = new FilterModel();
 
-const filters = generateFilter(pointsModel.points);
+const filterPresenter = new FilterPresenter(siteHeaderElement.querySelector('.trip-controls__filters'), filterModel, pointsModel);
+filterPresenter.init();
+const tripPresenter  = new TripEventsPresenter(siteMainElement.querySelector('.trip-events'), pointsModel, filterModel);
 
-render(new FiltersView(filters), siteHeaderElement.querySelector('.trip-controls__filters'));
+const newPointButtonComponent = new NewPointButtonView();
+
+const handleNewPointFormClose = () => {
+  newPointButtonComponent.element.disabled = false;
+};
+
+const handleNewPointButtonClick = () => {
+  tripPresenter.createNewForm(handleNewPointFormClose);
+  newPointButtonComponent.element.disabled = true;
+};
+
+render(newPointButtonComponent, siteHeaderElement);
+newPointButtonComponent.setClickHandler(handleNewPointButtonClick);
+
+render(new SiteMenuView(), siteHeaderElement.querySelector('.trip-controls__navigation'));
